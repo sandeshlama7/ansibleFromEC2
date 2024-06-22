cfa481aacb2ff79e3c54c990e5a62fac3dc8f677
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-06-22 16:27:36 +0545

    Fixed provisioner local-exec issues

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,46 +1,17 @@
 resource "aws_instance" "module-ec2" {
-    ami = var.ami
-    instance_type = var.instance_type
-    subnet_id = var.subnet_id
-    security_groups             = [aws_security_group.module-sg.id]
-    iam_instance_profile        = var.iam_profile
-    key_name = var.ssh-key
-    associate_public_ip_address = var.associate_public_ip
-    tags = {
-        Name    = var.names.ec2
-    }
-    user_data = var.user_data
-
-  provisioner "local-exec" {
-  command = <<EOT
-    scp -i /Users/sandesh/Downloads/Key-Sandesh.pem -o StrictHostKeyChecking=no -r ec2-ansible ubuntu@${self.public_ip}:/home/ubuntu/
-    ssh -i /Users/sandesh/Downloads/Key-Sandesh.pem -o StrictHostKeyChecking=no ubuntu@${self.public_ip} "ansible-playbook -i /home/ubuntu/ansible/inventory.ini /home/ubuntu/ansible/playbook.yml"
-  EOT
-}
-
-}
-
-resource "aws_security_group" "module-sg" {
-  name        = var.names.sg
-  description = "SG for the Public Instance"
-  vpc_id      = var.vpc_id
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids = [var.security_group]
+  iam_instance_profile        = var.iam_profile
+  key_name                    = var.ssh-key
+  associate_public_ip_address = var.associate_public_ip
   tags = {
-    Name = var.names.sg
+    Name = var.names.ec2
   }
+  # user_data = var.user_data
+}
 
-  ingress {
-    description = "Allow HTTP"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-
-  egress {
-    description = "Outbound Rules to Allow All Outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "public_ip" {
+  value = aws_instance.module-ec2.public_ip
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,49 +1,54 @@
 variable "ami" {
-    description = "EC2 AMI"
-    type = string
+  description = "EC2 AMI"
+  type        = string
 }
 
 variable "instance_type" {
-    description = "Instance type of the EC2 instance"
-    type = string
+  description = "Instance type of the EC2 instance"
+  type        = string
 }
 
 variable "names" {
-    description = "Names for different resources"
-    type = map(string)
-    default = {
-      "ec2" = "ec2-terraform-module-sandesh"
-      "sg" = "module-sg-sandesh"
-    }
+  description = "Names for different resources"
+  type        = map(string)
+  default = {
+    "ec2" = "ec2-terraform-module-sandesh"
+    "sg"  = "module-sg-sandesh"
+  }
 }
 
 variable "subnet_id" {
-    description = "Subnet ID of the subnet where our instance will be created"
-    type = string
+  description = "Subnet ID of the subnet where our instance will be created"
+  type        = string
 }
 
 variable "ssh-key" {
-    description = "Key to SSH into the EC2"
-    type = string
+  description = "Key to SSH into the EC2"
+  type        = string
 }
 
 variable "associate_public_ip" {
-    description = "To give public IP to the instance or not"
-    type = bool
-    default = false
+  description = "To give public IP to the instance or not"
+  type        = bool
+  default     = false
 }
 
-variable "vpc_id" {
-    description = "Id of the VPC"
-    type = string
-}
+# variable "vpc_id" {
+#   description = "Id of the VPC"
+#   type        = string
+# }
 
 variable "iam_profile" {
-    description = "IAM Role Name to attach to the EC2 to allow SSM"
-    type = string
+  description = "IAM Role Name to attach to the EC2 to allow SSM"
+  type        = string
 }
 
-variable "user_data" {
-  description = "User data for EC2"
+# variable "user_data" {
+#   description = "User data for EC2"
+#   type = string
+# }
+
+variable "security_group" {
+  description = "SG ID"
   type = string
 }
